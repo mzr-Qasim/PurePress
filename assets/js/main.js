0bd88ea9d4317e9de189c6afb7baae5e258e142e
Muhammad Qasim <malikqasim20051@gmail.com>
2025-01-21 12:08:19 +0500

    commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,19 +5,19 @@ let dashboardContentWidth = document.getElementById('dashboardcontentwidth');
 let icon = asideMenuButton.querySelector('.aside-menu-icon');
 
 asideMenuButton.addEventListener('click', function() {
-    // Toggle classes for asideSection and dashboardContentWidth
+
     asideSection.classList.toggle("asideMenu-smallwidth");
     dashboardContentWidth.classList.toggle("dashboard-content-width");
 
-    // Loop through the NodeList and toggle 'lg-hide-menu' on each element
+
     lgHide.forEach(function(item) {
         item.classList.toggle("lg-hide-menu");
     });
-    // if (icon.classList.contains('icon-back-to-icons-aside')) {
-    //     icon.classList.remove('icon-back-to-icons-aside');
-    //     icon.classList.add('icon-full-aside'); 
-    // } else {
-    //     icon.classList.remove('icon-full-aside');
-    //     icon.classList.add('icon-back-to-icons-aside');
-    // }
+    if (icon.classList.contains('icon-back-to-icons-aside')) {
+        icon.classList.remove('icon-back-to-icons-aside');
+        icon.classList.add('icon-full-aside'); 
+    } else {
+        icon.classList.remove('icon-full-aside');
+        icon.classList.add('icon-back-to-icons-aside');
+    }
 });
